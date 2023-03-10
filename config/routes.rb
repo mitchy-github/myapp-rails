77340e03eed088ee42e78bedbc79163c68200967
mitchy-github <mitchellqa8@ymail.ne.jp>
2023-03-10 22:37:30 +0900

    sessions#index追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :sessions, only: %i(index)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
