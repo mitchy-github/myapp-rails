74a492ba03266d43f31d55e0763e75165e937617
mitchy-github <mitchellqa8@ymail.ne.jp>
2023-03-20 14:56:06 +0900

    タイトルロゴの修正とコードの改修

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,34 +12,31 @@ before_action :set_user, only: [:show, :edit, :update, :destroy ]
       log_in @user
       redirect_to @user, notice: "新規登録完了しました。"
     else
-      render 'new', status: :unprocessable_entity
+      render "new", status: :unprocessable_entity
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     @user.avatar.attach(params[:user][:avatar]) if @user.avatar.blank?
-    if @user.email == 'guest@exapmle.com'
-      redirect_to :root, alert: "ゲストユーザーは編集できません。"
+    if @user.email == "guest@exapmle.com"
+      redirect_to root_url, alert: "ゲストユーザーは編集できません。"
     elsif
       @user.update(user_params)
       redirect_to user_url(@user), notice: "ユーザーアカウントを編集しました。"
     else
-      render :edit, status: :unprocessable_entity # rails7 から必須のオプション
+      render "edit", status: :unprocessable_entity # rails7 から必須のオプション
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     if @user.guest_user?
-      redirect_to :root, alert: "ゲストユーザーは登録解除できません。"
+      redirect_to root_url, alert: "ゲストユーザーは登録解除できません。"
     else
       @user.destroy
       redirect_to root_url, notice: "登録解除しました。", status: :see_other
