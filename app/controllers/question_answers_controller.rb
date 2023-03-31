8075a2386566d3e6605840af1a5bfd182f7fdc2f
mitchy-github <mitchellqa8@ymail.ne.jp>
2023-04-01 00:08:42 +0900

    2回目の実装内容変更

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -3,18 +3,17 @@ class QuestionAnswersController < ApplicationController
     @question_answer = QuestionAnswer.new(question_answer_params)
     @question_answer.user_id = current_user.id
     @question_answer.question_id = question_id
-    if @question_answer.save
-      flash[:notice] = "コメントしました。"
-      redirect_to question_path question_id
-    else
-      @question = Question.find(question_id)
-      render "questions/show", status: :unprocessable_entity
-    end
+    @question_answer.save!
+    flash[:notice] = "コメントしました。"
+    redirect_to question_path(question_id)
+  rescue StandardError
+    @question = Question.find(question_id)
+    render "questions/show", status: :unprocessable_entity
   end
 
   def destroy
     QuestionAnswer.find_by(id: params[:id],question_id: question_id).destroy
-    redirect_to ("/questions/#{question_id}"), notice: "コメントを解除しました。", status: :see_other
+    redirect_to question_path(question_id), notice: "コメントを解除しました。", status: :see_other
   end
 
   private
