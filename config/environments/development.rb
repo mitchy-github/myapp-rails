e5d33e1b3cdf3751ee764fce7bf1ac1bfcaa7b44
mitchy-github <mitchellqa8@ymail.ne.jp>
2024-08-05 21:31:57 +0900

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << '.example.com'
-  config.hosts << "alb-user1-569998420.us-west-2.elb.amazonaws.com"
-  config.hosts << "koukyouliving.net"
+  config.hosts.clear
 
   # config.action_controller.allow_forgery_protection = false
   # Settings specified here will take precedence over those in config/application.rb.
